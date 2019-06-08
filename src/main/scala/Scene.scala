c46dbb241be788f82999cfeefc33bdc2dc0bd837
Mateusz Hurbol <mateusz.hurbol42@gmail.com>
2019-06-08 16:26:43 +0200

    Added expressions values and conditions

--- src/main/scala/Scene.scala
+++ src/main/scala/Scene.scala
@@ -8,10 +8,50 @@ case class Dialog(expressions: List[Expression] ) extends ScenePart
 
 
 sealed abstract class Expression
+sealed abstract class Value extends Expression
+sealed abstract class Boolean extends Expression
+
 case class TODOExpression(todo: String ) extends Expression { //TODO EXPRESSIONS
     override def toString = s"Some expressions"
 }
 
+
+case class PositiveNoun(todo: String ) extends Value
+case class NegativeNoun(todo: String ) extends Value
+case class NeutralNoun(todo: String ) extends Value
+
+case class Adjective(value: Value) extends Value
+
+case class Sum(a: Value, b: Value) extends Value
+case class Difference(a: Value, b: Value) extends Value
+case class Product(a: Value, b: Value) extends Value
+case class Quotient(a: Value, b: Value) extends Value
+case class Remainder(a: Value, b: Value) extends Value
+
+case class Square(a: Value) extends Value
+case class SquareRoot(a: Value) extends Value
+
+case class CharacterValue(character: Character) extends Value
+
+case class Assigment(character: Character, value: Value) extends Expression
+
+case class PrintInt(dest: Character) extends Expression
+case class LoadInt(dest: Character) extends Expression
+case class PrintChar(dest: Character) extends Expression
+case class LoadChar(dest: Character) extends Expression
+
+case class GotoS(scene : Scene) extends Expression
+case class GotoA(scene : Act) extends Expression
+
+case class Push(dest: Character, src: Value) extends Expression
+case class Pop(dest: Character) extends Expression
+
+case class Equal(a: Value, b :Value) extends Boolean
+case class Less(a: Value, b :Value) extends Boolean
+case class More(a: Value, b :Value) extends Boolean
+case class Not(b: Boolean) extends Boolean
+
+case class ConditionalBlock(condition : Boolean, expression: Expression) extends Expression
 
 class Scene (val id : String, val sceneParts: List[ScenePart]){
 
