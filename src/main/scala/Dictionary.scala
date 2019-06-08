3b0e397a5dfdf5c03aeea2465b0114df397c6c2f
wicia <viktor145@wp.pl>
2019-06-08 15:32:52 +0200

    poczatek

---
+++ src/main/scala/Dictionary.scala
@@ -0,0 +1,65 @@
+import scala.collection.mutable
+
+
+class Dictionary ()
+{
+
+  val directory : String = "./words"
+
+  var  article: mutable.Set[String] = mutable.Set[String]()
+  var be: mutable.Set[String] = mutable.Set[String]()
+  var character: mutable.Set[String] = mutable.Set[String]()
+  var first_person: mutable.Set[String] = mutable.Set[String]()
+  var first_person_possessive: mutable.Set[String] = mutable.Set[String]()
+  var first_person_reflexive: mutable.Set[String] = mutable.Set[String]()
+  var negative_adjective: mutable.Set[String] = mutable.Set[String]()
+  var negative_comparative: mutable.Set[String] = mutable.Set[String]()
+  var negative_noun: mutable.Set[String] = mutable.Set[String]()
+  var neutral_adjective: mutable.Set[String] = mutable.Set[String]()
+  var neutral_noun: mutable.Set[String] = mutable.Set[String]()
+  var nothing: mutable.Set[String] = mutable.Set[String]()
+  var positive_adjective: mutable.Set[String] = mutable.Set[String]()
+  var positive_comparative: mutable.Set[String] = mutable.Set[String]()
+  var positive_noun: mutable.Set[String] = mutable.Set[String]()
+  var second_person: mutable.Set[String] = mutable.Set[String]()
+  var second_person_possessive: mutable.Set[String] = mutable.Set[String]()
+  var second_person_reflexive: mutable.Set[String] = mutable.Set[String]()
+  var third_person_possessive: mutable.Set[String] = mutable.Set[String]()
+
+  val roman_numerals_map : Map[String,Int] = Map("M" -> 1000, "D" -> 500,
+  "C" -> 100, "L" -> 50, "X" -> 10, "V" -> 5, "I" -> 1)
+
+
+
+
+
+
+
+
+
+
+  //mapa roman numerals na nasze
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
